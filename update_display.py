6c9be1aa5f1ef3962f29e31a55f5b1b646bcc3bf
Patrick Kanzler <patrick.kanzler@fablab.fau.de>
2016-06-15 14:02:20 +0200

    add trunk for python script

---
+++ update_display.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""bla
+
+:author: `Patrick Kanzler <patrick.kanzler@fau.fablab.de>`_ and `Michael Zapf <michi.zapf@fau.de>`_
+:organization: `FAU FabLab <https://github.com/fau-fablab>`_
+:copyright: Copyright (c) 2016 authors
+:license: MIT License
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
